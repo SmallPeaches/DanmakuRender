7d36d7a357080a08acaa799f62fca8c7b22b8130
SmallPeaches <65701756+SmallPeaches@users.noreply.github.com>
2022-01-13 18:00:00 +0800

    Add files via upload

--- downloader/getrealurl/__init__.py
+++ downloader/getrealurl/__init__.py
@@ -1,10 +1,22 @@
 import os
 import re
 
+def split_url(url:str):
+    platform = re.findall(r'\.(.*).com/',url)[0]
+    rid = re.findall(r'\.com/([\w]*)',url)[0]
+
+    if platform == 'douyu':
+        try:
+            int(rid)
+        except:
+            if 'rid=' in url:
+                rid = re.findall(r'rid=[0-9]*',url)[0][4:]
+    
+    return (platform, rid)
+
 def get_stream_url(live_url):
     stream_url = None
-    platform = re.findall(r'\.(.*).com/',live_url)[0]
-    rid = re.findall(r'\.com/([\w]*)',live_url)[0]
+    platform,rid = split_url(live_url)
     
     if platform in ['hy','huya']:
         from . import huya
