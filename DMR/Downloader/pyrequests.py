9112a1246a0a5b0feca1d65ca72b52335204344e
SmallPeaches <1097624849@qq.com>
2025-04-03 23:01:21 +0800

    修复强制原画录制存在的问题

--- DMR/Downloader/pyrequests.py
+++ DMR/Downloader/pyrequests.py
@@ -224,16 +224,17 @@ class PyRequestsDownloader:
             except Exception as e:
                 done, _ = wait(self.future_to_segid, timeout=1, return_when=FIRST_COMPLETED)
                 finished_segid_to_future = {self.future_to_segid[fut]: fut for fut in done}
-                with open(self.video_file, 'ab') as video_file:
-                    while self.next_segid in finished_segid_to_future:
-                        future = finished_segid_to_future[self.next_segid]
-                        result = future.result()
-                        if not result:
-                            self.logger.debug(f'{self.taskname} Error downloading segment: {self.next_segid}')
-                        else:
-                            video_file.write(result)
-                        self.next_segid += 1
-                        del self.future_to_segid[future]
+                if finished_segid_to_future:
+                    with open(self.video_file, 'ab') as video_file:
+                        while self.next_segid in finished_segid_to_future:
+                            future = finished_segid_to_future[self.next_segid]
+                            result = future.result()
+                            if not result:
+                                self.logger.debug(f'{self.taskname} Error downloading segment: {self.next_segid}')
+                            else:
+                                video_file.write(result)
+                            self.next_segid += 1
+                            del self.future_to_segid[future]
                 raise e
             finally:
                 if self.video_file and exists(self.video_file):
