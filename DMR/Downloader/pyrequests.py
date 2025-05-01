f6f32a100f8a2412687cd4c58249064491005c30
SmallPeaches <1097624849@qq.com>
2025-05-01 21:04:26 +0800

    优化pyrequests录制引擎

--- DMR/Downloader/pyrequests.py
+++ DMR/Downloader/pyrequests.py
@@ -16,12 +16,102 @@ from os.path import splitext, split, join, exists
 from DMR.utils import *
 
 
-class PyRequestsDownloader:
+class PyRequestsFlvDownloader:
     default_header = {
             'Content-Type': 'application/x-www-form-urlencoded',
             'User-Agent': 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 '
                             '(KHTML, like Gecko) Chrome/75.0.3770.100 Mobile Safari/537.36 '
         }
+
+    def __init__(self, 
+                 stream_url:str, 
+                 output_dir:str,
+                 output_format:str,
+                 segment:int,
+                 url:str,
+                 taskname:str,
+                 debug=False,
+                 header:dict=None,
+                 advanced_video_args:dict=None,
+                 segment_callback=None,
+                 **kwargs):
+        
+        self.stream_url = stream_url
+        self.header = header if header else self.default_header
+        self.output_dir = output_dir
+        self.output_format = output_format
+        self.segment = segment
+        self.debug = debug
+        self.taskname = taskname
+        self.url = url
+        self.segment_callback = segment_callback
+        self.advanced_video_args = advanced_video_args if advanced_video_args else {}
+        self.kwargs = kwargs
+
+        if '.flv' not in self.stream_url:
+            self.logger.warning(f'pyrequests仅支持flv流!')
+
+        self.stoped = False
+        self.logger = logging.getLogger(__name__)
+        self.session = requests.Session()
+        self.session.headers.update(self.header)
+
+    def _download_part(self, stream_iter:requests.Response):
+        start_t = datetime.now()
+        self.video_file = join(self.output_dir, f'[正在录制]{self.taskname}-{start_t.strftime("%Y%m%d-%H%M%S")}.{self.output_format}')
+        if exists(self.video_file):
+            cnt = len(glob.glob(splitext(self.video_file)[0] + '*'))
+            self.video_file = splitext(self.video_file)[0] + f'({cnt})' + splitext(self.video_file)[1]
+
+        with open(self.video_file, 'wb') as file_obj:
+            for idx, chunk in enumerate(stream_iter.iter_content(chunk_size=512*1024)):
+                file_obj.write(chunk)
+                if idx % 10 == 0:
+                    file_obj.flush()
+
+                now_t = datetime.now()
+                duration = (now_t - start_t).total_seconds()
+                if (self.segment and duration > self.segment) \
+                    or self.stoped:
+                    file_obj.flush()
+                    break
+
+        return self.video_file
+
+    def start(self):
+        self.logger.debug(f'{self.taskname} PyRequestsFlv: {self.stream_url}')
+
+        stream = self.session.get(self.stream_url, headers=self.header, stream=True)
+        if stream.status_code != 200:
+            raise RuntimeError(f'Error downloading stream: {stream.status_code}')
+        
+        while not self.stoped:
+            try:
+                self._download_part(stream)
+            except Exception as e:
+                self.logger.debug(f'{self.taskname} Error downloading stream: {e}')
+                raise e
+            finally:
+                if self.video_file and exists(self.video_file):
+                    new_file = rename_safe(self.video_file, self.video_file.replace('[正在录制]', ''))
+                    if not new_file:
+                        self.logger.error(f'{self.taskname} 重命名文件 {self.video_file} 失败!')
+                        new_file = self.video_file
+                    self.logger.debug(f'{self.taskname} 录制完成: {new_file}')
+                    self.segment_callback(new_file)
+    
+    def stop(self):
+        self.stoped = True
+        self.logger.debug('PyRequestsFlv downloader stoped.')
+
+
+class PyRequestsHlsDownloader:
+    default_header = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 '
+                            '(KHTML, like Gecko) Chrome/75.0.3770.100 Mobile Safari/537.36 '
+        }
+
     def __init__(self,  
                  stream_url:str, 
                  output_dir:str,
@@ -54,7 +144,7 @@ class PyRequestsDownloader:
         self.force_origin = self.advanced_video_args.get('force_origin', True)
 
         if 'bilibili' not in self.url or '.m3u8' not in self.stream_url:
-            self.logger.warning(f'pyrequests仅支持bilibili的m3u8流!')
+            self.logger.warning(f'pyrequestsh仅支持来自bilibili的hls流, 其他来源的hls流可能导致录制错误!')
             # raise ValueError(f'pyrequests仅支持bilibili的m3u8流!')
 
         self.downloaded_files = deque(maxlen=60)
@@ -105,12 +195,8 @@ class PyRequestsDownloader:
                 time.sleep(1)
 
     def _init_headfile(self):
-        m3u8_uri = self.stream_url
-        for _ in range(3):
-            m3u8_obj = m3u8.load(m3u8_uri, headers=self.header, timeout=3)
-            if m3u8_obj.segments:
-                break
-            m3u8_uri = m3u8_obj.playlists[0].absolute_uri
+        m3u8_uri = self.m3u8_uri
+        m3u8_obj = m3u8.load(m3u8_uri, headers=self.header, timeout=3)
 
         # 获取mp4文件头
         params_str = m3u8_uri.split('?')[1] if '?' in m3u8_uri else ''
@@ -132,14 +218,7 @@ class PyRequestsDownloader:
             cnt = len(glob.glob(splitext(self.video_file)[0] + '*'))
             self.video_file = splitext(self.video_file)[0] + f'({cnt})' + splitext(self.video_file)[1]
 
-        # 处理playlist的情况
-        m3u8_uri = self.stream_url
-        for _ in range(3):
-            m3u8_obj = m3u8.load(m3u8_uri, headers=self.header, timeout=3)
-            if m3u8_obj.segments:
-                break
-            m3u8_uri = m3u8_obj.playlists[0].absolute_uri
-
+        m3u8_uri = self.m3u8_uri
         params_str = m3u8_uri.split('?')[1] if '?' in m3u8_uri else ''
         
         m4s_duration = 0
@@ -211,7 +290,7 @@ class PyRequestsDownloader:
                 time.sleep(max(0, 3-time.time()+t0))
 
     def start(self):
-        self.logger.debug(f'{self.taskname} Pyrequests: {self.stream_url}')
+        self.logger.debug(f'{self.taskname} PyrequestsHls: {self.stream_url}')
         if self.force_origin and self._convert2origin(self.stream_url) != self.stream_url:
             self.logger.warning(f'{self.taskname} 正工作在强制原画模式下，可能导致录制错误!')
             self.logger.debug(self._convert2origin(self.stream_url))
@@ -220,6 +299,15 @@ class PyRequestsDownloader:
         self.segid = 0
         self.next_segid = 0
         self.future_to_segid = {}
+
+        # 处理playlist的情况
+        m3u8_uri = self.stream_url
+        for _ in range(3):
+            m3u8_obj = m3u8.load(m3u8_uri, headers=self.header, timeout=3)
+            if m3u8_obj.segments:
+                break
+            m3u8_uri = m3u8_obj.playlists[0].absolute_uri
+        self.m3u8_uri = m3u8_uri
 
         while not self.stoped:
             try:
@@ -253,3 +341,20 @@ class PyRequestsDownloader:
         self.stoped = True
         self.download_executor.shutdown(wait=False)
         self.logger.debug('Pyrequests downloader stoped.')
+
+
+class PyRequestsDownloader:
+    def __init__(self, stream_url:str, *args, **kwargs):
+        self.logger = logging.getLogger(__name__)
+        if '.m3u8' in stream_url or '.m3u' in stream_url:
+            self.logger.debug(f'即将使用PyRequestsHlsDownloader下载流: {stream_url}')
+            self.downloader = PyRequestsHlsDownloader(stream_url, *args, **kwargs)
+        else:
+            self.logger.debug(f'即将使用PyRequestsFlvDownloader下载流: {stream_url}')
+            self.downloader = PyRequestsFlvDownloader(stream_url, *args, **kwargs)
+    
+    def start(self):
+        self.downloader.start()
+    
+    def stop(self):
+        self.downloader.stop()
