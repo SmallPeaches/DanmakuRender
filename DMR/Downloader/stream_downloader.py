c39e26dbb33caf59bedd50e5a2028fde6ad9a87e
haha252 <2052249283@qq.com>
2025-12-21 03:25:01 +0800

    添加了WebUI，燃尽了

--- DMR/Downloader/stream_downloader.py
+++ DMR/Downloader/stream_downloader.py
@@ -3,6 +3,7 @@ import os
 import queue
 import threading
 import time
+import re
 
 from concurrent.futures import ThreadPoolExecutor, as_completed, TimeoutError
 from os.path import join,exists,splitext
@@ -50,6 +51,7 @@ class StreamDownloadTask():
         self.engine = engine or 'auto'
         self.advanced_video_args = advanced_video_args if advanced_video_args else {}
         self.advanced_dm_args = advanced_dm_args if advanced_dm_args else {}
+        self.stoped = True
 
         # if self.engine not in ['ffmpeg', 'streamlink', 'streamgears', 'pyrequests', 'auto']:
         #     raise NotImplementedError(f'No Downloader Named {self.engine}.')
